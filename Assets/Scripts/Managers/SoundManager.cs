69f677937c450034b677e683bd1a70c0199c9048
Barkın ZORLU <barkinzorlu@outlook.com>
2025-12-24 22:23:26 +0300

    Update SoundManager

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,10 +4,22 @@ using System;
 public partial class SoundManager : Node
 {
     public static SoundManager Instance { get; private set; }
-    
-    [Export] public AudioStreamPlayer2D MusicPlayer { get; private set; }
-    [Export] public AudioStreamPlayer2D SfxPlayer { get; private set; }
+    [Export] public AudioStreamPlayer MenuMusicPlayer { get; private set; }
 
+    /// <summary>
+    /// Player for music in the levels.
+    /// </summary>
+    [Export] public AudioStreamPlayer LevelMusicPlayer { get; private set; }
+    
+    /// <summary>
+    /// Player for sound effects, effects that include footsteps, explosions, and other in-game sounds.
+    /// </summary>
+    [Export] public AudioStreamPlayer EffectsPlayer { get; private set; }
+
+    /// <summary>
+    /// Master volume that affects all sounds. 0-100 range.
+    /// </summary>
+    public float MasterVolume { get; private set; }
     public float MusicVolume { get; private set; }
     public float SfxVolume { get; private set; }
 
@@ -19,19 +31,27 @@ public partial class SoundManager : Node
 
     public void SetVolumes()
     {
-        MusicPlayer.VolumeDb = Mathf.LinearToDb(MusicVolume);
-        SfxPlayer.VolumeDb = Mathf.LinearToDb(SfxVolume);
+        float masterEffect = MasterVolume / 100f;
+        MenuMusicPlayer.VolumeDb = Mathf.LinearToDb(masterEffect * MusicVolume);
+        LevelMusicPlayer.VolumeDb = Mathf.LinearToDb(masterEffect * MusicVolume);
+        EffectsPlayer.VolumeDb = Mathf.LinearToDb(masterEffect * SfxVolume);
     }
 
-    public void PlayMusic(AudioStream music)
+    public void PlayLevelMusic(AudioStream music)
     {
-        MusicPlayer.Stream = music;
-        MusicPlayer.Play();
+        if (MenuMusicPlayer.Playing)
+            MenuMusicPlayer.Stop();
+
+        LevelMusicPlayer.Stream = music;
+        LevelMusicPlayer.Play();
     }
 
     public void PlaySfx(AudioStream sfx)
     {
-        SfxPlayer.Stream = sfx;
-        SfxPlayer.Play();
+        if (EffectsPlayer.Playing)
+            EffectsPlayer.Stop();
+
+        EffectsPlayer.Stream = sfx;
+        EffectsPlayer.Play();
     }
 }
