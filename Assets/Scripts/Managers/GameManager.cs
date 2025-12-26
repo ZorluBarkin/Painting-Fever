0b14429a01fb3e6ba1bef71bf38e032fa6926805
Gokhan-IRTEM <gokirtem@gmail.com>
2025-12-26 20:10:20 +0300

    BPM ler
    müzik bpmlei

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,66 +3,66 @@ using System;
 
 public partial class GameManager : Node, IEventSubscriber
 {
-    public static GameManager Instance { get; private set; }
-    public static event Action<GameState, GameState> GameStateChanged;
+	public static GameManager Instance { get; private set; }
+	public static event Action<GameState, GameState> GameStateChanged;
 
-    [Export] private PackedScene GUI;
-    [Export] public Node2D GameSceneRoot { get; private set; }
-    public GameState CurrentGameState { get; private set; } = GameState.Start;
+	[Export] private PackedScene GUI;
+	[Export] public Node2D GameSceneRoot { get; private set; }
+	public GameState CurrentGameState { get; private set; } = GameState.Start;
 
-    public override void _Ready()
-    {
-        Instance = this;
-        InitiateGUI();
-        ((IEventSubscriber)this).SubscribeToEvents();
-    }
+	public override void _Ready()
+	{
+		Instance = this;
+		InitiateGUI();
+		((IEventSubscriber)this).SubscribeToEvents();
+	}
 
-    public override void _ExitTree()
-    {
-        ((IEventSubscriber)this).UnsubscribeFromEvents();
-    }
+	public override void _ExitTree()
+	{
+		((IEventSubscriber)this).UnsubscribeFromEvents();
+	}
 
-    void IEventSubscriber.SubscribeToEvents()
-    {
-        GameManager.GameStateChanged += OnGameStateChanged;
-    }
+	void IEventSubscriber.SubscribeToEvents()
+	{
+		GameManager.GameStateChanged += OnGameStateChanged;
+	}
 
-    void IEventSubscriber.UnsubscribeFromEvents()
-    {
-        GameManager.GameStateChanged -= OnGameStateChanged;
-    }
+	void IEventSubscriber.UnsubscribeFromEvents()
+	{
+		GameManager.GameStateChanged -= OnGameStateChanged;
+	}
 
-    private async void InitiateGUI()
-    {
-        await ToSignal(GetTree().CreateTimer(0.5f), SceneTreeTimer.SignalName.Timeout);
-        GetTree().Root.AddChild(GUI.Instantiate());
-    }
+	private async void InitiateGUI()
+	{
+		await ToSignal(GetTree().CreateTimer(0.5f), SceneTreeTimer.SignalName.Timeout);
+		GetTree().Root.AddChild(GUI.Instantiate());
+	}
 
-    public static void ChangeGameState(GameState oldState, GameState targetState)
-    {
-        #if DEBUG
-        GD.Print($"Game State changed from {oldState} to {targetState}");
-        #endif
-        GameStateChanged?.Invoke(oldState, targetState);
-    }
+	public static void ChangeGameState(GameState oldState, GameState targetState)
+	{
+		#if DEBUG
+		GD.Print($"Game State changed from {oldState} to {targetState}");
+		#endif
+		GameStateChanged?.Invoke(oldState, targetState);
+	}
 
-    public static void OnGameStateChanged(GameState oldState, GameState targetState)
-    {
-        switch (targetState)
-        {
-            case GameState.Start:
-                break;
-            case GameState.loading:
-                break;
-            case GameState.Play:
-                break;
-            case GameState.Paused:
-                break;
-            case GameState.End:
-                break;
-            default:
-                break;
-        }
-        Instance.CurrentGameState = targetState;
-    }
+	public static void OnGameStateChanged(GameState oldState, GameState targetState)
+	{
+		switch (targetState)
+		{
+			case GameState.Start:
+				break;
+			case GameState.loading:
+				break;
+			case GameState.Play:
+				break;
+			case GameState.Paused:
+				break;
+			case GameState.End:
+				break;
+			default:
+				break;
+		}
+		Instance.CurrentGameState = targetState;
+	}
 }
--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -3,51 +3,51 @@ using System;
 
 public partial class LevelManager : Node
 {
-    public static LevelManager Instance { get; private set; }
+	public static LevelManager Instance { get; private set; }
 
-    public Level CurrentLevel { get; private set; }
-    public PackedScene TargetLevel { get; private set; }
+	public Level CurrentLevel { get; private set; }
+	public PackedScene TargetLevel { get; private set; }
 
-    [Export] public LevelData LevelData { get; private set; }
-    [Export] public ColorData ColorData { get; private set; }
+	[Export] public LevelData LevelData { get; private set; }
+	[Export] public ColorData ColorData { get; private set; }
 
-    public event Action<Level> LevelLoaded;
+	public event Action<Level> LevelLoaded;
 
-    public LevelManager() { Instance = this; }
+	public LevelManager() { Instance = this; }
 
-    public void InstantiateLevel(Difficulty difficulty, int index)
-    {
-        TargetLevel = null;
-        switch (difficulty)
-        {
-            case Difficulty.Easy:
-                if (index < LevelData.EasyLevelScenes.Count)
-                    TargetLevel = LevelData.EasyLevelScenes[index];
-                break;
-            case Difficulty.Medium:
-                if (index < LevelData.MediumLevelScenes.Count)
-                    TargetLevel = LevelData.MediumLevelScenes[index];
-                break;
-            case Difficulty.Hard:
-                if (index < LevelData.HardLevelScenes.Count)
-                    TargetLevel = LevelData.HardLevelScenes[index];
-                break;
-            case Difficulty.EasterEgg:
-                if (index < LevelData.EasterEggLevelScenes.Count)
-                    TargetLevel = LevelData.EasterEggLevelScenes[index];
-                break;
-        }
-        // TODO: Temporary remove once levels are implemented
-        TargetLevel = LevelData.DevLevels[0];
+	public void InstantiateLevel(Difficulty difficulty, int index)
+	{
+		TargetLevel = null;
+		switch (difficulty)
+		{
+			case Difficulty.Easy:
+				if (index < LevelData.EasyLevelScenes.Count)
+					TargetLevel = LevelData.EasyLevelScenes[index];
+				break;
+			case Difficulty.Medium:
+				if (index < LevelData.MediumLevelScenes.Count)
+					TargetLevel = LevelData.MediumLevelScenes[index];
+				break;
+			case Difficulty.Hard:
+				if (index < LevelData.HardLevelScenes.Count)
+					TargetLevel = LevelData.HardLevelScenes[index];
+				break;
+			case Difficulty.EasterEgg:
+				if (index < LevelData.EasterEggLevelScenes.Count)
+					TargetLevel = LevelData.EasterEggLevelScenes[index];
+				break;
+		}
+		// TODO: Temporary remove once levels are implemented
+		TargetLevel = LevelData.DevLevels[0];
 
-        if (TargetLevel == null)
-        {
-            GD.PrintErr($"Level Doesn't Exist for Difficulty: {difficulty}, Index: {index}");
-            return;
-        }
+		if (TargetLevel == null)
+		{
+			GD.PrintErr($"Level Doesn't Exist for Difficulty: {difficulty}, Index: {index}");
+			return;
+		}
 
-        CurrentLevel = TargetLevel.Instantiate<Level>();
-        GameManager.Instance.GameSceneRoot.AddChild(CurrentLevel);
-        LevelLoaded.Invoke(CurrentLevel);
-    }
+		CurrentLevel = TargetLevel.Instantiate<Level>();
+		GameManager.Instance.GameSceneRoot.AddChild(CurrentLevel);
+		LevelLoaded.Invoke(CurrentLevel);
+	}
 }
