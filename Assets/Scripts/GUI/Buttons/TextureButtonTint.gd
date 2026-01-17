e48a0f293f97d5530aa6e8bfd9ed6c640e0f72f8
Barkın ZORLU <barkinzorlu@outlook.com>
2026-01-17 18:12:39 +0300

    Dynamic swictch for label on tinted buttons

--- Assets/Scripts/GUI/Buttons/TextureButtonTint.gd
+++ Assets/Scripts/GUI/Buttons/TextureButtonTint.gd
@@ -5,40 +5,44 @@ extends TextureButton
 @export var normal_color: Color = Color.LIGHT_GRAY
 
 @export_category("Label colors")
-@export var label_hover_color: Color = Color.BLACK
-@export var label_normal_color: Color = Color.WHITE
+@export var label_light_color: Color = Color.BLACK
+@export var label_dark_color: Color = Color.WHITE
 
 var _label: Label = null
 
 func _ready():
+	normal_color = self_modulate
+	
 	# get the label if there is one
 	_get_label()
-	normal_color = self_modulate
+	
 	mouse_entered.connect(_on_hover)
 	mouse_exited.connect(_on_normal)
 	button_down.connect(_on_pressed)
 	button_up.connect(_on_hover)
 
 func _get_label():
-		# find label
 	for child in find_children("*", "Label"):
 		if child is Label:
 			_label = child
-			_label.self_modulate = label_normal_color
+			_update_label_color()
 			break;
 
-func _update_label_color(new_color: Color):
-	if _label:
-		_label.self_modulate = new_color
+func _update_label_color():
+	if _label: # null check
+		if (self_modulate.get_luminance() > 0.6):
+			_label.self_modulate = label_dark_color
+		else:
+			_label.self_modulate = label_light_color
 
 func _on_hover():
 	self_modulate = hover_color
-	_update_label_color(label_hover_color)
+	_update_label_color()
 
 func _on_normal():
 	self_modulate = normal_color
-	_update_label_color(label_normal_color)
-	
+	_update_label_color()
+
 func _on_pressed():
 	self_modulate = pressed_color
-	_update_label_color(label_normal_color)
+	_update_label_color()
